b1eda64f71cb4c376bd03c0ec46c049d3f6097f0
Brekke-Green <brekke.andrew.green@gmail.com>
2024-09-12 13:19:45 -0400

    Change color styles for relaxing and energizing according to figma mockup

--- public/styleEnergizing.js
+++ public/styleEnergizing.js
@@ -5,15 +5,17 @@ if (document.getElementById("energizingElement")) {
     document.getElementById("energizingElement").remove()
 } else {
     const footerElement = document.getElementsByClassName("global-footer-compact")[0]
-    if (footerElement) {
-        document.getElementsByClassName("scaffold-layout__sidebar")[0].appendChild(footerElement)
-    }
+    // if (footerElement) {
+    //     if (document.getElementsByClassName("global-footer-compact")) {
+    //         document.getElementsByClassName("scaffold-layout__sidebar")[0].appendChild(footerElement)
+    //     }
+    // }
     const energizingElement = document.createElement("STYLE");
     energizingElement.setAttribute("id", "energizingElement")
     let energizingStyle = document.createTextNode(`
-        body {background-color: #d6e6fe !important;}
+        body {background-color: #FF6F61 !important;}
         .scaffold-layout__list {margin-right: 50px;}
-        .scaffold-layout__main {background-color: #d6e6fe ; grid-column-end: aside;}
+        .scaffold-layout__main {background-color: #FF6F61 ; grid-column-end: aside;}
         .scaffold-layout__list-container {background-color: #f2ffff;}
         .scaffold-layout__detail {background-color: #f2ffff;}
         .jobs-search-results-list__header--blue {background-color: #d3e6ff !important;}
